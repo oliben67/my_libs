5e30e40efa02c998f429b5e90cf4b099112f3df4
Olivier Steck <48995182+oliben67@users.noreply.github.com>
2024-05-02 09:00:43 -0400

    Update inserts.py: adding support fot strings

--- lists/inserts.py
+++ lists/inserts.py
@@ -1,10 +1,12 @@
 from itertools import groupby
 
+
 def splitted(lst, elem):
     """
     Split list on a given element
     """
-    return [list(group) for k, group in groupby(lst, lambda x: x==elem) if not k]
+    return [list(group) for k, group in groupby(lst, lambda x: x == elem) if not k]
+
 
 def flatten(lst):
     """
@@ -32,9 +34,17 @@ def insert_at(lst, sub, elem, _all=False):
     :type _all: ``bool``
     :rtype: ``lst``
     """
+    if isinstance(lst, str):
+        return insert_at(
+            [c for c in lst],
+            [c for c in sub] if isinstance(sub, str) else sub,
+            elem,
+            _all=_all,
+        )
+
     def _insert_at(l):
         idx = l.index(elem)
-        return l[: idx] + sub + l[idx + 1:]
+        return l[:idx] + sub + l[idx + 1 :]
 
     if not _all:
         return _insert_at(lst)
@@ -46,6 +56,7 @@ def insert_at(lst, sub, elem, _all=False):
             break
 
     return lst
+
 
 def insert_after(lst, sub, elem, _all=False):
     """
@@ -62,19 +73,34 @@ def insert_after(lst, sub, elem, _all=False):
     :type _all: ``bool``
     :rtype: ``lst``
     """
+    if isinstance(lst, str):
+        return insert_after(
+            [c for c in lst],
+            [c for c in sub] if isinstance(sub, str) else sub,
+            elem,
+            _all=_all,
+        )
+
     if not _all:
         idx = lst.index(elem)
-        return lst[: idx + 1] + sub + lst[idx + 1:]
-    
-    split_lst =  [list(group if not k else []) for k, group in groupby(lst, lambda x: x==elem)]
-    return flatten([sub_list + [elem] + sub  if not sub_list else sub_list for sub_list in split_lst])
+        return lst[: idx + 1] + sub + lst[idx + 1 :]
+
+    split_lst = [
+        list(group if not k else []) for k, group in groupby(lst, lambda x: x == elem)
+    ]
+    return flatten(
+        [
+            sub_list + [elem] + sub if not sub_list else sub_list
+            for sub_list in split_lst
+        ]
+    )
 
 
 def insert_before(lst, sub, elem, _all=False):
     """
     Inserts a sublist `sub` before the first occurrence of `elem` in the given list `lst`.
     If `_all` is True, inserts `sub` before all occurrences of `elem` in `lst`.
-    
+
     :param lst: The list to insert into.
     :type lst: ``lst``
     :param sub: The list to insert.
@@ -85,9 +111,24 @@ def insert_before(lst, sub, elem, _all=False):
     :type _all: ``bool``
     :rtype: ``lst``
     """
+    if isinstance(lst, str):
+        return insert_before(
+            [c for c in lst],
+            [c for c in sub] if isinstance(sub, str) else sub,
+            elem,
+            _all=_all,
+        )
+
     if not _all:
         idx = lst.index(elem)
-        return lst[: idx] + sub + lst[idx:]
-    
-    split_lst =  [list(group if not k else []) for k, group in groupby(lst, lambda x: x==elem)]
-    return flatten([sub + [elem] + sub_list if not sub_list else sub_list for sub_list in split_lst])
+        return lst[:idx] + sub + lst[idx:]
+
+    split_lst = [
+        list(group if not k else []) for k, group in groupby(lst, lambda x: x == elem)
+    ]
+    return flatten(
+        [
+            sub + [elem] + sub_list if not sub_list else sub_list
+            for sub_list in split_lst
+        ]
+    )
